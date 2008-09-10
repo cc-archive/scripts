c3628fc0e8bf4bd9be1818c5674804b72088d2cd
nkinkade <nkinkade@committer.creativecommons.org>
2008-09-10 23:13:18 +0000

    Removed logging

--- civicrm2commoner.py
+++ civicrm2commoner.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+"""Script for pulling contributions out of the CiviCRM database
+that have a status of completed and are between some arbitary
+point in the past and the moment the script is run.
+"""
+
 import sys
 import time
 import datetime
-import logging
 import sqlalchemy
 
 if len(sys.argv) > 1:
@@ -28,16 +32,13 @@ db = sqlalchemy.create_engine('mysql://root@localhost/civicrm', convert_unicode=
 metadata = sqlalchemy.MetaData(db)
 crm_contribs = sqlalchemy.Table('civicrm_contribution', metadata, autoload=True)
 
-# configure the logger.  see: http://docs.python.org/lib/module-logging.html
-logging.basicConfig(
-	format='%(levelname)-8s %(message)s',
-	filename='civicrm2commoner.log',
-	filemode='w'
-)
-
 recv_date = crm_contribs.c.receive_date
 status = crm_contribs.c.contribution_status_id
 
 contributions = crm_contribs.select(sqlalchemy.and_(recv_date > get_date, status == 1)).execute().fetchall() 
-for contribution in contributions:
-    print contribution
+
+if len(contributions):
+    for contribution in contributions:
+        print contribution
+else:
+    return None
