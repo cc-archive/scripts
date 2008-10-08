a8d53c90ddd471cacbe8407504a6ac5c55148d3c
nkinkade <nkinkade@committer.creativecommons.org>
2008-10-08 21:19:54 +0000

    Added script to mail Newsletter and Events subscription requests so they can be added to the mailmain lists

---
+++ get_civicrm_signups.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import re
+import os
+import datetime
+import sqlalchemy
+
+rcpt_to = 'nkinkade@gmail.com'
+date = datetime.date.today().isoformat()
+letters = []
+events = []
+
+# setup database connectivity
+db = sqlalchemy.create_engine('mysql://root@localhost/civicrm_staging', convert_unicode=True)
+metadata = sqlalchemy.MetaData(db)
+tbl_signups = sqlalchemy.Table('civicrm_value_1_mailings_6', metadata, autoload=True)
+tbl_email = sqlalchemy.Table('civicrm_email', metadata, autoload=True)
+
+c_id = tbl_signups.c.id
+c_letter = tbl_signups.c.newletter
+c_events = tbl_signups.c.events_new
+c_emailid = tbl_email.c.contact_id
+
+signups = tbl_signups.select(sqlalchemy.or_(c_letter == 'yes', c_events == 'yes')).execute().fetchall()
+
+for signup in signups:
+    email = tbl_email.select(c_emailid == signup[2]).execute().fetchone()
+    if re.search("yes", signup[3]):
+        letters.append(email[3])
+	tbl_signups.update(c_id == signup[0], {'newletter':''}).execute()
+    if re.search("yes", signup[4]):
+        events.append(email[3])
+	tbl_signups.update(c_id == signup[0], {'events_new':''}).execute()
+
+# If there were any new subscription requests
+if letters or events:
+    p = os.popen("/usr/sbin/sendmail -t", "w")
+    p.write('To: %s\n' % rcpt_to)
+    p.write('From: civicrm@support.creativecommons.org\n')
+    p.write('Subject: Mail list subscriptions for %s \n' % date)
+    p.write('\n')
+    p.write('Newsletter subscriptions:\n')
+    p.write('\n'.join(letters))
+    p.write('\n\n')
+    p.write('Events subscriptions:\n')
+    p.write('\n'.join(events))
+    status = p.close()
